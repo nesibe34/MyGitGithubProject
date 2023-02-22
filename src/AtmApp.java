23801fc08ed2dbe2b81efc0c7360377ff2ae3bfb
Nesibe <tnesibe.techproed@gmail.com>
2023-02-22 21:29:53 +0300

    v2

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -1,5 +1,7 @@
 public class AtmApp {
     public static void main(String[] args) {
         System.out.println("Bu bir ATM projesidir.");
+        //version2
+        System.out.println("Java is easy");
     }
 }
